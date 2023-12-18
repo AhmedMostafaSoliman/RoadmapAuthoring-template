d507787e66bd7e896ad7585c62702304542b0ab9
Ahmed Mostafa <ahmedmostafa72@hotmail.com>
2023-12-18 01:08:21 -0800

    added basket functionality + sound feedback and removed the small blue box that was originally used for menu

--- Assets/Scripts/BerrySelector.cs
+++ Assets/Scripts/BerrySelector.cs
@@ -10,21 +10,43 @@ public class BerrySelector : MonoBehaviour
     // Reference to your text element (Assign this from the Inspector)
     public TextMeshPro scoreText;
 
+    public GameObject basket;
+
+    public AudioSource ErrorSound;
+    public AudioSource CorrectSound;
+
     void Awake()
     {
         grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.onSelectEntered.AddListener(HandleGrab);
+        grabInteractable.onSelectExited.AddListener(HandleRelease);
     }
 
     private void HandleGrab(XRBaseInteractor interactor)
     {
+        if (gameObject.CompareTag("WormyBerry"))
+        {
+            ErrorSound.Play();
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void HandleRelease(XRBaseInteractor interactor)
+    {
         if (gameObject.CompareTag("GoodBerry"))
         {
-            score++;
-            UpdateScoreDisplay(); // Update the score display
+            // check if game object coordinates is within the basket coordinates
+            if (gameObject.transform.position.x >= basket.transform.position.x - 0.8f && gameObject.transform.position.x <= basket.transform.position.x + 0.8f)
+            {
+                if (gameObject.transform.position.z >= basket.transform.position.z - 0.8f && gameObject.transform.position.z <= basket.transform.position.z + 0.8f)
+                {
+                    CorrectSound.Play();
+                    score += 1;
+                    UpdateScoreDisplay();
+                    //gameObject.SetActive(false);
+                }
+            }
         }
-
-        gameObject.SetActive(false);
     }
 
     private void UpdateScoreDisplay()
